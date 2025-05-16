40ec2532150555d84b570deffe75e1ccc8b67ad8
Moataz Ghazy <m.ghazy44@hotmail.com>
2025-05-16 12:39:02 -0400

    commit

--- components/PricingButton.jsx
+++ components/PricingButton.jsx
@@ -10,10 +10,10 @@ export default function PricingButton({ children, email, active, id, className }
 
   async function handleClick() {
     if (id === "year") {
-      const url = await createCheckout(email, "price_1RPR2rG2GpyU7y3qPwmDkOnj")
+      const url = await createCheckout(email, "price_1RPAlx4fERSy0thU9aydx6CO")
       return redirect(url)
     }
-    const url = await createCheckout(email, "price_1RPR2DG2GpyU7y3qEB6WHIf1")
+    const url = await createCheckout(email, "price_1RPAkg4fERSy0thUD0tQy5XE")
     redirect(url)
   }
   
