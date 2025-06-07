9ae2c6089527bda352fedc32bfd3f1241ca6ea75
Moataz Ghazy <m.ghazy44@hotmail.com>
2025-06-06 21:01:24 -0400

    commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import connectDB from "@/utils/db"
 import { cookies } from "next/headers"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "@/app/globals.css"
 
 export const metadata = {
@@ -16,6 +17,7 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en" className={lightMode?.value === "true"? "" : "dark"}>
+      {!process.env.NEXT_PUBLIC_ORIGIN?.includes("localhost")? <GoogleAnalytics gaId="G-CMQL4V6Y7F" /> : <></>}
       <body className="bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-white">
         {children}
       </body>
