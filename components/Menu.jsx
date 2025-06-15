c7c999f258e8f4e38bbf5326fb16b1c060138d87
Moataz Ghazy <m.ghazy44@hotmail.com>
2025-06-15 17:45:51 -0400

    commit

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -7,6 +7,11 @@ export default async function Menu() {
   const user = await getUser()
   
   return (
-    <MenuClient isLightMode={isLightMode} user={user? true : null}/>
+    <>
+      <p className="text-sm font-semibold text-white bg-blue-500 px-3 py-2 text-center text-base">
+        🎉 Use code <code className="bg-white text-sky-700 font-bold px-2 py-1 rounded">FIRST50</code> for 50% off – limited to the first 50 users!
+      </p>
+      <MenuClient isLightMode={isLightMode} user={user? true : null}/>
+    </>
   )
 }
